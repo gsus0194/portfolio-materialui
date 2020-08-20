60d097e707f5f76509a120de65627cc53572ca96
gsus0194 <j.villegascambero@gmail.com>
2020-08-20 13:15:13 -0500

    Updates

---
+++ precache-manifest.7176f54f3945faec8aa11d32b6fe0b6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4df6ee1c28a7623ccd55e58efad7b236",
+    "url": "/portfolio-materialui/index.html"
+  },
+  {
+    "revision": "b92fd332aec70eca180a",
+    "url": "/portfolio-materialui/static/js/2.8acf7bd3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio-materialui/static/js/2.8acf7bd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69b156033208b8efc5ae",
+    "url": "/portfolio-materialui/static/js/main.481908a8.chunk.js"
+  },
+  {
+    "revision": "56e44b71696b4e150dd2",
+    "url": "/portfolio-materialui/static/js/runtime-main.70871d33.js"
+  }
+]);
